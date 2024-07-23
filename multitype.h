8922e56aecb432f879a4e006bc44ae6f365b32ba
Klaus <direjor2010@yandex.ru>
2024-07-23 20:02:39 +0300

    Update multitype.h
    add comment with unsafe version without initialization and works by stack forward hack

--- multitype.h
+++ multitype.h
@@ -41,4 +41,34 @@ void multitypeClose(){ _MT_deleter(pthread_getspecific(_MT_key)); pthread_key_de
 
 #endif // MULTITYPE_H
 
+/*
+#ifndef MULTITYPE_H
+#define MULTITYPE_H
 
+#include "macro.h"
+
+typedef char multitype;
+
+#define let(...)       M_IF(M_COUNT(M_REMAINED(__VA_ARGS__)))(_MT_LET,M_EXTRACT)(__VA_ARGS__)
+#define return(...)    M_IF(M_COUNT(M_REMAINED(__VA_ARGS__)))(_MT_RETURN,return)(__VA_ARGS__)
+#define multitype(...) M_IF(M_COUNT(__VA_ARGS__))( M_IF(M_COUNT(M_REMAINED(__VA_ARGS__)))(multitype,__VA_ARGS__),multitype)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define _MT_TYPEOF(...) __typeof__(__VA_ARGS__)
+#define _MT_UNPACK(_0_,_1_,...) M_WHEN(M_IS_ARG(__VA_ARGS__))( __VA_ARGS__=*(_MT_TYPEOF(__VA_ARGS__)*)(*_1_); ) ++_1_;
+#define _MT_PACK(_1_,...) M_FOREACH(__MT_PACK,_1_,__VA_ARGS__)
+#define __MT_PACK(_index_,_1_,...) M_WHEN(M_IS_ARG(__VA_ARGS__))( _1_->M_JOIN(_,_index_)=(__VA_ARGS__); *_1_->i=&_1_->M_JOIN(_,_index_); ++_1_->i; )
+#define _MT_STRUCT(...) struct{void *p[_MT_COUNT(__VA_ARGS__)]; M_FOREACH(__MT_STRUCT,-,__VA_ARGS__) void **i;}
+#define __MT_STRUCT(_index_,_0_,...) M_WHEN(M_IS_ARG(__VA_ARGS__))( _MT_TYPEOF(__VA_ARGS__) M_JOIN(_,_index_); )
+#define _MT_COUNT(...) (M_FOREACH(__MT_COUNT,-,__VA_ARGS__) 0)
+#define __MT_COUNT(_1_,_2_,...) M_WHEN(M_IS_ARG(__VA_ARGS__))( 1+ )
+#define _MT_ADDR(_addr_) __MT_ADDR(_addr_,512)
+#define __MT_ADDR(_addr_,_align_) ((void*)( ((1+(size_t)_addr_) + (_align_ - 1)) & -_align_ ))
+#define _MT_LET(...) for(;;({goto M_JOIN(_mark,M_LINE());}))if(!8){M_JOIN(_mark,M_LINE()): if(1){void **_1_=_MT_ADDR(&_1_); M_FOREACH(_MT_UNPACK,_1_,__VA_ARGS__) break;}} else *({char _1_; (&_1_);})
+#define _MT_RETURN(...) { _MT_STRUCT(__VA_ARGS__) *_1_=_MT_ADDR(&_1_); _1_->i=(void*)_1_;_MT_PACK(_1_,__VA_ARGS__) } return 0;
+
+#endif // MULTITYPE_H
+
+*/
